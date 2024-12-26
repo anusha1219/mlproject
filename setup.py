527d861873abb4476e41378058e77414dc7de8d3
Anusha Gunnam <anusha.gunnam0411@gmail.com>
2024-12-26 07:09:42 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+    '''
+    HYPEN_E_DOT ='-e .'
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
     name='mlproject',
@@ -6,6 +20,5 @@ setup(
     author='Anusha',
     author_email='anusha.gunnam0411@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
-    
+    install_requires=get_requirements('requirements.txt')
 )
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_details(error, error_details:sys):
@@ -13,9 +13,3 @@ class CustomException(Exception):
         self.error_message=error_message_details(error_message,error_details=error_details)
     def __str__(self):
         return self.error_message
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Diveded by Zero")
-        raise CustomException(e,sys)
